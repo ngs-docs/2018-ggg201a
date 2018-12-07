46e9df9315cd786b061af4086eb9b93f546effe3
Taylor Reiter <taylorreiter@gmail.com>
2018-12-06 19:25:10 -0800

    rm sf

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 install.packages("tidyverse")
 install.packages("vegan")
 install.packages("pheatmap")
-install.packages("sf")
+#install.packages("sf")
 source("https://bioconductor.org/biocLite.R")
 BiocInstaller::biocLite(c("GenomicAlignments", "VariantAnnotation", 
 			  "Biostrings", "GenomicFeatures"))
