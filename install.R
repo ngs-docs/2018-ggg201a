5a7a12dc3eccea9f0f7c902fff63baa33a3c5006
C. Titus Brown <titus@idyll.org>
2019-09-27 06:36:40 -0700

    update install

--- install.R
+++ install.R
@@ -1,7 +1,10 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocInstaller::install(c("GenomicAlignments", "VariantAnnotation", "Biostrings", "GenomicFeatures"))
+
 install.packages("ggplot2")
 install.packages("tidyr")
 install.packages("dplyr")
 install.packages("vegan")
 install.packages("pheatmap")
-source("https://bioconductor.org/biocLite.R")
-BiocInstaller::biocLite(c("GenomicAlignments", "VariantAnnotation", "Biostrings", "GenomicFeatures"))
